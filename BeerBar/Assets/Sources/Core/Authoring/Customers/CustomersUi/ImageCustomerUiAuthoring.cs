e91edf4516a1e6e24cf9ef335ecfafa8845abb3b
Nirotom <progx21.7@yandex.ru>
2025-01-09 22:29:01 +0300

    Revert "Add new customer emotion icon, procuct and UpgradeAndEventButton enable,disable function"
    This reverts commit d3dba11d9fab4bbb439a757b4aea48e6a7331d67.

---
+++ BeerBar/Assets/Sources/Core/Authoring/Customers/CustomersUi/ImageCustomerUiAuthoring.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Core.Authoring.Customers.CustomersUi
+{
+    public sealed class ImageCustomerUiAuthoring : EntityBehaviour
+    {
+        [SerializeField] private Image _image;
+        
+        public Image FaceEmotionImage => _image;
+        
+        public CanvasGroup CanvasGroupFaceEmotion => _canvasGroup;
+
+        [SerializeField]
+        private CanvasGroup _canvasGroup;
+    }
+}
